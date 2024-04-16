3d89737f68d2b2887d12a53383c8c37250919612
Luis Urbitsch <luis@edus1691.hs-esslingen.de>
2024-04-16 10:16:35 +0200

    add hello world rest api route

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func helloWorld(context *gin.Context) {
+	context.JSON(200, gin.H{"message": "Hello World"})
+}
 
 func main() {
+	router := gin.Default()
+	router.GET("/helloWorld", helloWorld)
 	fmt.Print("Hello World")
+	router.Run()
 }
