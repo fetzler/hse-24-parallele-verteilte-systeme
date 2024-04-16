8de7849322f7829b543e8c3036162496514c3f86
Luis Urbitsch <luis@Luiss-MacBook-Pro.local>
2024-04-16 20:49:04 +0200

    update connection string

--- backend/main.go
+++ backend/main.go
@@ -14,7 +14,7 @@ func helloWorld(context *gin.Context) {
 }
 
 func main() {
-	conn, err := pgx.Connect(context.Background(), "postgres://postgres:postgres@db:5432")
+	conn, err := pgx.Connect(context.Background(), "postgres://postgres:postgres@db:5432/todo_list")
 	if err != nil {
 		fmt.Println(err)
 		log.Fatalln("error connecting to the database.")
@@ -22,6 +22,5 @@ func main() {
 	defer conn.Close(context.Background())
 	router := gin.Default()
 	router.GET("/helloWorld", helloWorld)
-	fmt.Print("Hello World")
 	router.Run()
 }
