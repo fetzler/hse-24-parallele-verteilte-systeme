66a1f3fa16d4bfaac84c682dd9dc127019b5e07b
Luis Urbitsch <luis@Luiss-MacBook-Pro.local>
2024-04-18 15:50:27 +0200

    add api endpoint to creat todo item

--- backend/main.go
+++ backend/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -12,63 +11,11 @@ import (
 )
 
 func createTodoItem(c *gin.Context) {
-	_, err := conn.Exec(context.Background(), "INSERT INTO tasks (title) VALUES ($1)", c.Param("title"))
+	_, err := conn.Query(context.Background(), "INSERT INTO tasks (title) VALUES ($1)", c.Param("title"))
 	if err != nil {
 		log.Println(err)
-		c.String(http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
-		return
 	}
-	c.String(http.StatusCreated, http.StatusText(http.StatusCreated))
-}
-
-func updateTodoItem(c *gin.Context) {
-	rows, err := conn.Exec(context.Background(), "UPDATE tasks SET title = $1 WHERE task_id = $2", c.Param("title"), c.Param("id"))
-	if err != nil {
-		log.Println(err)
-		c.String(http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
-		return
-	}
-	if rows.RowsAffected() == 0 {
-		log.Println("No TodoItem found for this ID")
-		c.String(http.StatusNotFound, http.StatusText(http.StatusNotFound))
-	}
-	c.String(http.StatusOK, http.StatusText(http.StatusOK))
-}
-
-func getTodoItemById(c *gin.Context) {
-	rows, _ := conn.Query(context.Background(), "SELECT task_id, title FROM tasks WHERE task_id = $1", c.Param("id"))
-	todoItem, err := pgx.CollectExactlyOneRow(rows, pgx.RowToStructByName[TodoItem])
-	rows.Close()
-	if err != nil {
-		log.Println(err)
-		if errors.Is(err, pgx.ErrNoRows) {
-			c.String(http.StatusNotFound, http.StatusText(http.StatusNotFound))
-		}
-		return
-	}
-	c.JSON(http.StatusOK, todoItem)
-}
-
-func getAllTodoItems(c *gin.Context) {
-	rows, _ := conn.Query(context.Background(), "SELECT task_id, title FROM tasks")
-	todoItems, err := pgx.CollectRows(rows, pgx.RowToStructByName[TodoItem])
-	rows.Close()
-	if err != nil {
-		log.Println(err)
-		c.String(http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
-		return
-	}
-	c.JSON(http.StatusOK, todoItems)
-}
-
-func deleteTodoItem(c *gin.Context) {
-	_, err := conn.Exec(context.Background(), "DELETE FROM tasks WHERE task_id = $1", c.Param("id"))
-	if err != nil {
-		log.Println(err)
-		c.String(http.StatusInternalServerError, http.StatusText((http.StatusInternalServerError)))
-		return
-	}
-	c.String(http.StatusOK, http.StatusText(http.StatusOK))
+	c.String(http.StatusOK, "TodoItem was added")
 }
 
 var conn *pgx.Conn
@@ -83,9 +30,5 @@ func main() {
 	defer conn.Close(context.Background())
 	router := gin.Default()
 	router.POST("/todos/:title", createTodoItem)
-	router.PUT("/todos/:id/:title", updateTodoItem)
-	router.GET("/todos/:id", getTodoItemById)
-	router.GET("/todos", getAllTodoItems)
-	router.DELETE("/todos/:id", deleteTodoItem)
 	router.Run()
 }
--- backend/todo_item.go
+++ backend/todo_item.go
@@ -1,6 +1,11 @@
 package main
 
 type TodoItem struct {
+<<<<<<< HEAD
 	Id    uint32 `json:"id" db:"task_id"`
 	Title string `json:"title" db:"title"`
+=======
+	Id    uint32 `json:"Id"`
+	Title string `json:"Title"`
+>>>>>>> add api endpoint to creat todo item
 }
