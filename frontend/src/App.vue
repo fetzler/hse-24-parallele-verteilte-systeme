85cb01727e85e1022afdef5f92b80ba309258b09
fetzler <164873005+fetzler@users.noreply.github.com>
2024-04-19 19:00:53 +0000

    Begun testing for frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,48 +1,19 @@
 <template>
-  <div>
-    <div class="header">
-      <h1 class="title">Most beautiful of all Java Todo Lists</h1>
-    </div>
-    <div class="add-todo">
-      <h2>Add a Todo</h2>
-      <div class="todo-input">
-        <input type="text" v-model="newTodo" placeholder="Enter Todo Description">
-        <button @click="addTodo">Submit!</button>
-      </div>
-    </div>
-    <div class="todo-list">
-      <h2>Todo List</h2>
-      <div v-for="(todo, index) in todos" :key="index" class="todo-item">
-        <span class="item">{{ todo.description }}</span>
-        <button @click="markAsDone(index)">Done!</button>
-      </div>
-    </div>
-  </div>
+  <h1>Ich bin hier</h1>
+  <button @click="add">Klick</button>
+  <h1>{{ database }}</h1>
+
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      newTodo: '',
-      todos: []
-    };
-  },
-  methods: {
-    addTodo() {
-      if (this.newTodo.trim() !== '') {
-        this.todos.push({
-          description: this.newTodo,
-          done: false
-        });
-        this.newTodo = '';
-      }
-    },
-    markAsDone(index) {
-      this.todos.splice(index,1);
-    }
+<script setup>
+  import { ref } from 'vue'
+  import axios from 'axios'
+
+  const database = ref(null)
+
+  function add() {
+    axios.get("http://backend:8080/todos").then(res => { database.value = res.data})
   }
-};
 </script>
 
 <style>
