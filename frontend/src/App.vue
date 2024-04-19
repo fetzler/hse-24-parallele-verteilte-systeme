e5b4c527d1f7242dbffb14b44dfd2149787bf349
Fabi <fabian.etzler@outlook.com>
2024-04-19 22:29:52 +0200

    Changed implementation of nginx

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <h1>Ich bin hier</h1>
   <button @click="add">Klick</button>
+  <button @click="remove">Remove</button>
   <h1>{{ database }}</h1>
 
 </template>
@@ -12,7 +13,16 @@
   const database = ref(null)
 
   function add() {
-    axios.get("http://backend:8080/todos").then(res => { database.value = res.data})
+    axios.get("/todos").then(res => { 
+    database.value = res.data;
+    }).catch(error => {
+      console.error('Error fetching todos:', error);
+    });
+  }
+  function remove() {
+    axios.post("/todos/mach").catch(error => {
+      console.error('Error posting todos', error);
+    })
   }
 </script>
 
