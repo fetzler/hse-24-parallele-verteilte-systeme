ba49c67b9464f50567b2390bbe0a636b73006baa
Luis Urbitsch <luis@Luiss-MacBook-Pro.local>
2024-04-19 11:02:51 +0200

    update api endpoints

--- backend/main.go
+++ backend/main.go
@@ -12,8 +12,7 @@ import (
 )
 
 func createTodoItem(c *gin.Context) {
-	rows, err := conn.Query(context.Background(), "INSERT INTO tasks (title) VALUES ($1)", c.Param("title"))
-	rows.Close()
+	_, err := conn.Exec(context.Background(), "INSERT INTO tasks (title) VALUES ($1)", c.Param("title"))
 	if err != nil {
 		log.Println(err)
 		c.String(http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
@@ -23,20 +22,25 @@ func createTodoItem(c *gin.Context) {
 }
 
 func updateTodoItem(c *gin.Context) {
-	rows, err := conn.Query(context.Background(), "UPDATE tasks SET title = $1 WHERE task_id = $2", c.Param("title"), c.Param("id"))
-	rows.Close()
+	rows, err := conn.Exec(context.Background(), "UPDATE tasks SET title = $1 WHERE task_id = $2", c.Param("title"), c.Param("id"))
 	if err != nil {
 		log.Println(err)
 		c.String(http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
 		return
 	}
+	if rows.RowsAffected() == 0 {
+		log.Println("No TodoItem found for this ID")
+		c.String(http.StatusNotFound, http.StatusText(http.StatusNotFound))
+	}
 	c.String(http.StatusOK, http.StatusText(http.StatusOK))
 }
 
 func getTodoItemById(c *gin.Context) {
-	rows, _ := conn.Query(&gin.Context{}, "SELECT task_id, title FROM tasks WHERE task_id = $1", c.Param("id"))
+	rows, _ := conn.Query(context.Background(), "SELECT task_id, title FROM tasks WHERE task_id = $1", c.Param("id"))
 	todoItem, err := pgx.CollectExactlyOneRow(rows, pgx.RowToStructByName[TodoItem])
+	rows.Close()
 	if err != nil {
+		log.Println(err)
 		if errors.Is(err, pgx.ErrNoRows) {
 			c.String(http.StatusNotFound, http.StatusText(http.StatusNotFound))
 		}
@@ -46,13 +50,25 @@ func getTodoItemById(c *gin.Context) {
 }
 
 func getAllTodoItems(c *gin.Context) {
-	rows, _ := conn.Query(&gin.Context{}, "SELECT task_id, title FROM tasks")
+	rows, _ := conn.Query(context.Background(), "SELECT task_id, title FROM tasks")
 	todoItems, err := pgx.CollectRows(rows, pgx.RowToStructByName[TodoItem])
+	rows.Close()
 	if err != nil {
+		log.Println(err)
 		c.String(http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
 		return
 	}
 	c.JSON(http.StatusOK, todoItems)
+}
+
+func deleteTodoItem(c *gin.Context) {
+	_, err := conn.Exec(context.Background(), "DELETE FROM tasks WHERE task_id = $1", c.Param("id"))
+	if err != nil {
+		log.Println(err)
+		c.String(http.StatusInternalServerError, http.StatusText((http.StatusInternalServerError)))
+		return
+	}
+	c.String(http.StatusOK, http.StatusText(http.StatusOK))
 }
 
 var conn *pgx.Conn
@@ -70,5 +86,6 @@ func main() {
 	router.PUT("/todos/:id/:title", updateTodoItem)
 	router.GET("/todos/:id", getTodoItemById)
 	router.GET("/todos", getAllTodoItems)
+	router.DELETE("/todos/:id", deleteTodoItem)
 	router.Run()
 }
